5a1d012f13bc390d16e2b1da9ab6ac08bbc9901f
ldjennings <jenningsliamd@gmail.com>
2024-09-01 04:06:37 -0400

    Risky business, probably should not be doing this at 4 am

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,10 @@
       ./hardware-configuration.nix
       ./gc.nix
       ./virtualization.nix
+      (import ./gnome.nix {
+        inherit pkgs;
+       })
+ 
     ];
 
 
@@ -57,48 +61,6 @@
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services = {
-    xserver = {
-      enable = true;
-      desktopManager.gnome = {
-        enable = true;
-        extraGSettingsOverridePackages = [pkgs.gnome.mutter];
-        extraGSettingsOverrides = ''
-          [org.gnome.mutter]
-	  experimental-features=['scale-monitor-framebuffer']
-        '';
-      };
-      displayManager.gdm.enable = true;
-    };
-  };
-
-    services.gnome =
-    {
-      core-utilities.enable = false;
-    };
-
-    environment.gnome.excludePackages = with pkgs;
-    [
-      gnome-tour
-    ];
-
-    # Add some actually useful packages back
-    environment.systemPackages = with pkgs;
-    [
-      gnome-extension-manager
-    ]
-    ++
-    (with pkgs.gnome;
-    [
-      gnome-tweaks
-      nautilus
-      gnome-terminal
-      gnome-disk-utility
-      resources
-    ]
-    );
 
 
   services.fprintd.enable = true;	 
---
+++ gnome.nix
@@ -0,0 +1,53 @@
+{
+  pkgs,
+  lib,
+  config,
+  hostVars,
+  ...
+}:
+
+{
+  # Enable the GNOME Desktop Environment.
+  services = {
+    xserver = {
+      enable = true;
+      desktopManager.gnome = {
+        enable = true;
+        extraGSettingsOverridePackages = [pkgs.gnome.mutter];
+        extraGSettingsOverrides = ''
+          [org.gnome.mutter]
+	  experimental-features=['scale-monitor-framebuffer']
+        '';
+      };
+      displayManager.gdm.enable = true;
+    };
+  };
+
+    services.gnome =
+    {
+      core-utilities.enable = false;
+    };
+
+    environment.gnome.excludePackages = with pkgs;
+    [
+      gnome-tour
+    ];
+
+    # Add some actually useful packages back
+    environment.systemPackages = with pkgs;
+    [
+      gnome-extension-manager
+    ]
+    ++
+    (with pkgs.gnome;
+    [
+      gnome-tweaks
+      nautilus
+      gnome-terminal
+      gnome-disk-utility
+      resources
+    ]
+    );
+
+
+}

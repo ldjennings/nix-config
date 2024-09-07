66ffca4f7631df2d78bd32119c9b4a66b50b7614
ldjennings <jenningsliamd@gmail.com>
2024-09-07 03:20:55 -0400

    Organize services further

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -68,39 +68,6 @@
      ];
    };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # fingerprint sensor service
-  services.fprintd.enable = true;	 
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "colemak";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.liam = {
@@ -113,8 +80,52 @@
     ];
   };
 
+  # Enable sound with pipewire. Configured further in the services section
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  
 
+  # All active user configured services on the system
+  services = {
+    # Enabling flatpak for everest and other programs
+    flatpak.enable = true;
+    
+    # fingerprint sensor service
+    fprintd.enable = true;	 
+    
+    # Enable CUPS to print documents
+    printing.enable = true;
+    
+    # Basic keyboard config
+    xserver.xkb = {
+      layout = "us";
+      variant = "colemak";
+    };
 
+    # Periodic SSD Trimming
+    fstrim.enable = true;
+
+    # Bios updates
+    fwupd.enable = true;
+
+    # Audio
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+  
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
+    # Base Touchpad support. This should usually be enabled by default.
+    # xserver.libinput.enable = true;
+
+};
 
 
 
@@ -130,18 +141,18 @@
 
   programs.steam.enable = true;
 
-  services.flatpak.enable = true;
+  # services.flatpak.enable = true;
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
 
 
-  services.fstrim.enable = true; # Disk
+ # services.fstrim.enable = true; # Disk
 
-  services.fwupd = # Bios updates
-  {
-    enable = true;
-  };
+#  services.fwupd = # Bios updates
+#  {
+#    enable = true;
+#  };
 
 
   # Some programs need SUID wrappers, can be configured further or are

4cd6284f44745170a4f06eb4bef7251415dcba77
ldjennings <jenningsliamd@gmail.com>
2024-09-26 23:56:36 -0400

    Adding keyring + formatting

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -1,50 +1,40 @@
-{
-  pkgs,
-  ...
-}:
-
-{
+{pkgs, ...}: {
   # Enable the GNOME Desktop Environment.
   services = {
-    gnome-keyring.enable = true;
     xserver = {
       enable = true;
       desktopManager.gnome = {
         enable = true;
         extraGSettingsOverridePackages = [pkgs.gnome.mutter];
         extraGSettingsOverrides = ''
-          [org.gnome.mutter]
-	  experimental-features=['scale-monitor-framebuffer']
+                 [org.gnome.mutter]
+          experimental-features=['scale-monitor-framebuffer']
         '';
       };
       displayManager.gdm.enable = true;
     };
   };
 
-    services.gnome =
-    {
-      core-utilities.enable = false;
-    };
+  services.gnome = {
+    core-utilities.enable = false;
+    gnome-keyring.enable = true;
+  };
 
-    environment.gnome.excludePackages = with pkgs;
-    [
-      gnome-tour
-    ];
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+  ];
 
-    # Add some actually useful packages back
-    environment.systemPackages = with pkgs;
+  # Add some actually useful packages back
+  environment.systemPackages = with pkgs;
     [
     ]
-    ++
-    (with pkgs.gnome;
-    [
-      gnome-tweaks
-      nautilus
-      gnome-terminal
-      gnome-disk-utility
-      resources
-    ]
+    ++ (
+      with pkgs.gnome; [
+        gnome-tweaks
+        nautilus
+        gnome-terminal
+        gnome-disk-utility
+        resources
+      ]
     );
-
-
 }

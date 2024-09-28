fb9631ebf0e89190029e9ae0f686d62aa57e4c47
ldjennings <jenningsliamd@gmail.com>
2024-09-27 20:07:21 -0400

    trying out grub

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -31,9 +31,14 @@
     ];
 
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.consoleMode = "0";
-  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.consoleMode = "0";
+  # boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "nodev";
+    boot.loader.grub.useOSProber = true;
+    boot.loader.grub.efiSupport = true;
 
   networking.hostName = "selfdestruction"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
